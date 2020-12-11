4f59cfc97c0da95daa1775626e0bc4004af90c5d
Ryan Owens <RyanOwens@linux.com>
2020-12-11 10:36:39 -0600

    chore: added debug information

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { EventPayloads } from "@octokit/webhooks";
 async function run() {
     try {
         core.debug("Starting PR Title check for Jira Issue Key");
+        core.info(JSON.stringify(github.context));
         const title = getPullRequestTitle();
         const regex = getRegex();
 
