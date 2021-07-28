772da562fad265dabd6516b7ac03ed4e6def8272
wtruong <wtruong@doxel.ai>
2021-07-28 15:31:30 -0700

    push package up

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -4,64 +4,67 @@ import { readFileSync } from "fs";
 import { getPullRequestTitle, getRegex } from "../src";
 
 describe("index", () => {
-    describe("getPullRequestTitle", () => {
-        beforeEach(() => {
-            delete process.env["GITHUB_EVENT_PATH"];
-        });
-        it("can get the title from the context", () => {
-            process.env["GITHUB_EVENT_PATH"] = __dirname + "/valid-context.json";
-            github.context.payload = JSON.parse(
-                readFileSync(process.env.GITHUB_EVENT_PATH, { encoding: 'utf8' })
-            );
-            const title = getPullRequestTitle();
-            expect(title).toBe("Test Title");
-        });
-
-        it("raises an exception if the event is not for a pull_request", () => {
-            process.env["GITHUB_EVENT_PATH"] = __dirname + "/wrong-event-type-context.json";
-            github.context.payload = JSON.parse(
-                readFileSync(process.env.GITHUB_EVENT_PATH, { encoding: 'utf8' })
-            );
-            expect(getPullRequestTitle).toThrowError("This action should only be run with Pull Request Events");
-        })
+  describe("getPullRequestTitle", () => {
+    beforeEach(() => {
+      delete process.env["GITHUB_EVENT_PATH"];
+    });
+    it("can get the title from the context", () => {
+      process.env["GITHUB_EVENT_PATH"] = __dirname + "/valid-context.json";
+      github.context.payload = JSON.parse(
+        readFileSync(process.env.GITHUB_EVENT_PATH, { encoding: "utf8" })
+      );
+      const title = getPullRequestTitle();
+      expect(title).toBe("Test Title");
     });
 
-    describe("getRegex", () => {
-        const name = "projectKey";
-        it("gets the default when no project key is provided", () => {
-            process.env[`INPUT_${name.replace(/ /g, '_').toUpperCase()}`] = "";
-            const regex = getRegex();
-            let defaultRegex = /(?<=^|[a-z]\-|[\s\p{Punct}&&[^\-]])([A-Z][A-Z0-9_]*-\d+)(.)+/;
-            expect(regex).toEqual(defaultRegex);
-            expect(regex.test("PR-4 this is valid")).toBe(true);
-        });
+    it("raises an exception if the event is not for a pull_request", () => {
+      process.env["GITHUB_EVENT_PATH"] =
+        __dirname + "/wrong-event-type-context.json";
+      github.context.payload = JSON.parse(
+        readFileSync(process.env.GITHUB_EVENT_PATH, { encoding: "utf8" })
+      );
+      expect(getPullRequestTitle).toThrowError(
+        "This action should only be run with Pull Request Events"
+      );
+    });
+  });
 
-        it("uses a project key if it exists", () => {
-            process.env[`INPUT_${name.replace(/ /g, '_').toUpperCase()}`] = "AB";
-            const regex = getRegex();
-            expect(regex).toEqual(new RegExp(`(^AB-){1}(\\d)+(\\s)+(.)+`));
-            expect(regex.test("AB-43 stuff and things")).toBe(true);
-        });
-
-        it("throws an exception if the provided project key is not valid", () => {
-            process.env[`INPUT_${name.replace(/ /g, '_').toUpperCase()}`] = "aB";
-            expect(getRegex).toThrowError("Project Key  \"aB\" is invalid");
-        });
+  describe("getRegex", () => {
+    const name = "projectKey";
+    it("gets the default when no project key is provided", () => {
+      process.env[`INPUT_${name.replace(/ /g, "_").toUpperCase()}`] = "";
+      const regex = getRegex();
+      let defaultRegex = /(?<=^|[a-z]\-|[\s\p{Punct}&&[^\-]])([A-Z][A-Z0-9_]*-\d+)(.)+/;
+      expect(regex).toEqual(defaultRegex);
+      expect(regex.test("PR-4 this is valid")).toBe(true);
     });
 
-    describe("testRegex", () => {
-        const name = "projectKey";
-        it("No whitespace between JIRA issue key and description is valid", () => {
-            process.env[`INPUT_${name.replace(/ /g, '_').toUpperCase()}`] = "";
-            const regex = getRegex();
-            expect(regex.test("PR-4:this is valid")).toBe(true);
-            expect(regex.test("PR-4_this is valid")).toBe(true);
-            expect(regex.test("PR-4/this is valid")).toBe(true);
-            expect(regex.test("PR-4-this is valid")).toBe(true);
-            expect(regex.test("PR-4: this is valid")).toBe(true);
-            expect(regex.test("PR-4_ this is valid")).toBe(true);
-            expect(regex.test("PR-4/ this is valid")).toBe(true);
-            expect(regex.test("PR-4- this is valid")).toBe(true);
-        });
+    it("uses a project key if it exists", () => {
+      process.env[`INPUT_${name.replace(/ /g, "_").toUpperCase()}`] = "AB";
+      const regex = getRegex();
+      expect(regex).toEqual(new RegExp(`(^AB-){1}(\\d)+(\\s)+(.)+`));
+      expect(regex.test("AB-43 stuff and things")).toBe(true);
     });
+
+    it("throws an exception if the provided project key is not valid", () => {
+      process.env[`INPUT_${name.replace(/ /g, "_").toUpperCase()}`] = "aB";
+      expect(getRegex).toThrowError('Project Key  "aB" is invalid');
+    });
+  });
+
+  describe("testRegex", () => {
+    const name = "projectKey";
+    it("No whitespace between JIRA issue key and description is valid", () => {
+      process.env[`INPUT_${name.replace(/ /g, "_").toUpperCase()}`] = "";
+      const regex = getRegex();
+      expect(regex.test("PR-4:this is valid")).toBe(true);
+      expect(regex.test("PR-4_this is valid")).toBe(true);
+      expect(regex.test("PR-4/this is valid")).toBe(true);
+      expect(regex.test("PR-4-this is valid")).toBe(true);
+      expect(regex.test("PR-4: this is valid")).toBe(true);
+      expect(regex.test("PR-4_ this is valid")).toBe(true);
+      expect(regex.test("PR-4/ this is valid")).toBe(true);
+      expect(regex.test("PR-4- this is valid")).toBe(true);
+    });
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -3,47 +3,50 @@ import * as github from "@actions/github";
 import { EventPayloads } from "@octokit/webhooks";
 
 async function run() {
-    try {
-        core.debug("Starting PR Title check for Jira Issue Key");
-        const title = getPullRequestTitle();
-        const regex = getRegex();
+  try {
+    core.debug("Starting PR Title check for Jira Issue Key");
+    const title = getPullRequestTitle();
+    const regex = getRegex();
 
-        core.debug(title);
-        core.debug(regex.toString());
+    core.debug(title);
+    core.debug(regex.toString());
 
-        if (!regex.test(title)) {
-            core.debug(`Regex ${regex} failed with title ${title}`);
-            core.info("Title Failed");
-            core.setFailed("PullRequest title does not start with a Jira Issue key.");
-            return;
-        }
-        core.info("Title Passed");
-
-    } catch (error) {
-        core.setFailed(error.message);
+    if (!regex.test(title)) {
+      core.debug(`Regex ${regex} failed with title ${title}`);
+      core.info("Title Failed");
+      core.setFailed("PullRequest title does not start with a Jira Issue key.");
+      return;
     }
+    core.info("Title Passed");
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 export function getRegex() {
-    let regex = /(?<=^|[a-z]\-|[\s\p{Punct}&&[^\-]])([A-Z][A-Z0-9_]*-\d+)(.)+/;
-    const projectKey = core.getInput("projectKey", { required: false });
-    if (projectKey && projectKey !== "") {
-        core.debug(`Project Key ${projectKey}`);
-        if (!/(?<=^|[a-z]\-|[\s\p{Punct}&&[^\-]])([A-Z][A-Z0-9_]*)/.test(projectKey)) {
-            throw new Error(`Project Key  "${projectKey}" is invalid`)
-        }
-        regex = new RegExp(`(^${projectKey}-){1}(\\d)+(\\s)+(.)+`);
+  let regex = /(?<=^|[a-z]\-|[\s\p{Punct}&&[^\-]])([A-Z][A-Z0-9_]*-\d+)(.)+/;
+  const projectKey = core.getInput("projectKey", { required: false });
+  if (projectKey && projectKey !== "") {
+    core.debug(`Project Key ${projectKey}`);
+    if (
+      !/(?<=^|[a-z]\-|[\s\p{Punct}&&[^\-]])([A-Z][A-Z0-9_]*)/.test(projectKey)
+    ) {
+      throw new Error(`Project Key  "${projectKey}" is invalid`);
     }
-    return regex;
+    regex = new RegExp(`(^${projectKey}-){1}(\\d)+(\\s)+(.)+`);
+  }
+  return regex;
 }
 
 export function getPullRequestTitle() {
-    let pull_request = github.context.payload.pull_request;
-    core.debug(`Pull Request: ${JSON.stringify(github.context.payload.pull_request)}`);
-    if (pull_request == undefined || pull_request.title == undefined) {
-        throw new Error("This action should only be run with Pull Request Events");
-    }
-    return pull_request.title;
+  let pull_request = github.context.payload.pull_request;
+  core.debug(
+    `Pull Request: ${JSON.stringify(github.context.payload.pull_request)}`
+  );
+  if (pull_request == undefined || pull_request.title == undefined) {
+    throw new Error("This action should only be run with Pull Request Events");
+  }
+  return pull_request.title;
 }
 
-run()
+run();
