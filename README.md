e08f0d9dc0315f1485ed9320950717209c06b3f5
Ryan Owens <ryanvader82@gmail.com>
2023-11-23 22:15:19 -0600

    refactor(additional features): add separator and anywhere in title features
    - bump action to Node.js 20
    - update dependencies
    - add optional separator parameter
    - add optional keyAnywhereInTitle parameter
    - update docs
    - add eslint config file
    
    BREAKING CHANGE: Now requires Node.js 20

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This action analyses the titles of Pull Requests to ensure they start with a Jir
 For example, if your project key were `AB` then the following would be allowed
 
 ```
-AB-1  Initialize Project
+AB-1 Initialize Project
 ```
 
 However, the following examples would not be allowed
@@ -28,26 +28,79 @@ Valid Pull Request titles must also include a short description after the Issue 
 AB-1
 ```
 
+Specifying a separator allows for using characters such as `:`, `-` or `_` to be between the Jira key and title. 
+As an example with the separator `:` the following is allowed
+
+```
+AB-1:Initialize Project
+```
+
+Note that by specifying a separator the following would not be allowed
+
+```
+AB-1: Initialize Project
+```
+
+By default this action checks that the Jira Project Key is at the start of the title string. To allow for the Jira Project
+Key to be anywhere in the title set the `keyAnywhereInTitle` property to `true`. With this property enabled and the project
+key set to `AB` the following title is valid
+
+```
+Other: AB-1 Initialize Project
+```
+
 By default, this action will allow any valid Issue Key so long as it *could* be valid. If you want to be specific to your project, use the `projectKey` input for the action. 
 
 ## Inputs
 
 ### `projectKey`
 
-A specific Project Key to always check for. 
+A specific Project Key to always check for. Defaults to a space character.
+
+### `separator`
+
+A specific separator to use 
+
+### `keyAnywhereInTitle`
+
+Allows the Jira Project Key, Issue # and separator to be anywhere in the title. Defaults to false.
 
 ## Example Usage
 
 ```
 - name: Enforce Jira Issue Key in Pull Request Title
-  uses: ryanvade/enforce-pr-title-style-action@v1
+  uses: ryanvade/enforce-pr-title-style-action@v2
 ```
 
 ## Example Usage with a specific Project Key
 
 ```
 - name: Enforce Jira Issue Key in Pull Request Title
-  uses: ryanvade/enforce-pr-title-style-action@v1
+  uses: ryanvade/enforce-pr-title-style-action@v2
   with:
-    projectKey: AB
+    projectKey: 'AB'
 ```
+
+## Example Usage with a specific Project Key and a separator
+
+```
+- name: Enforce Jira Issue Key in Pull Request Title
+  uses: ryanvade/enforce-pr-title-style-action@v2
+  with:
+    projectKey: 'AB'
+    separator: ':'
+```
+
+## Example Usage with a specific Project Key and allowed to be anywhere in the title
+
+```
+- name: Enforce Jira Issue Key in Pull Request Title
+  uses: ryanvade/enforce-pr-title-style-action@v2
+  with:
+    projectKey: 'AB'
+    keyAnywhereInTitle: 'true'
+```
+
+Note that edits to a pull request title will not include the updated title as an input to this action
+without specifying the `edited` type. See the [pull_request](https://docs.github.com/en/actions/using-workflows/events-that-trigger-workflows#pull_request)
+event trigger documentation for more details. Thanks to @jdonboch for pointing this out.
--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,102 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
-import { EventPayloads } from "@octokit/webhooks";
 
-async function run() {
-    try {
-        core.debug("Starting PR Title check for Jira Issue Key");
-        const title = getPullRequestTitle();
-        const regex = getRegex();
+const run = async () => {
+  try {
+    core.debug("Starting PR Title check for Jira Issue Key");
+    const title = getPullRequestTitle();
+    const regex = getRegex();
 
-        core.debug(title);
-        core.debug(regex.toString());
+    core.debug(title);
+    core.debug(regex.toString());
 
-        if (!regex.test(title)) {
-            core.debug(`Regex ${regex} failed with title ${title}`);
-            core.info("Title Failed");
-            core.setFailed("PullRequest title does not start with a Jira Issue key.");
-            return;
-        }
-        core.info("Title Passed");
-
-    } catch (error: any) {
-        core.setFailed(error.message);
+    if (!regex.test(title)) {
+      core.debug(`Regex ${regex} failed with title ${title}`);
+      core.info("Title Failed");
+      core.setFailed("PullRequest title does not start with a Jira Issue key.");
+      return;
     }
-}
+    core.info("Title Passed");
+    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+};
 
-export function getRegex() {
-    let regex = /(?<=^|[a-z]\-|[\s\p{Punct}&&[^\-]])([A-Z][A-Z0-9_]*-\d+)(?![^\W_])(\s)+(.)+/;
-    const projectKey = core.getInput("projectKey", { required: false });
-    if (projectKey && projectKey !== "") {
-        core.debug(`Project Key ${projectKey}`);
-        if (!/(?<=^|[a-z]\-|[\s\p{Punct}&&[^\-]])([A-Z][A-Z0-9_]*)/.test(projectKey)) {
-            throw new Error(`Project Key  "${projectKey}" is invalid`)
-        }
-        regex = new RegExp(`(^${projectKey}-){1}(\\d)+(\\s)+(.)+`);
-    }
-    return regex;
-}
+export const getRegex = () => {
+  const projectKey = core.getInput("projectKey", { required: false });
+  const separator = core.getInput("separator", { required: false });
+  const keyAnywhereInTitle = core.getBooleanInput("keyAnywhereInTitle", {
+    required: false,
+  });
 
-export function getPullRequestTitle() {
-    let pull_request = github.context.payload.pull_request;
-    core.debug(`Pull Request: ${JSON.stringify(github.context.payload.pull_request)}`);
-    if (pull_request == undefined || pull_request.title == undefined) {
-        throw new Error("This action should only be run with Pull Request Events");
-    }
-    return pull_request.title;
-}
+  core.debug(`Project Key ${projectKey}`);
+  core.debug(`Separator ${separator}`);
+  core.debug(`Key Anywhere In Title ${keyAnywhereInTitle}`);
 
-run()
+  if (!projectKey || projectKey === "") return getDefaultJiraIssueRegex();
+
+  if (!isValidProjectKey(projectKey))
+    throw new Error(`Project Key  "${projectKey}" is invalid`);
+
+  if (!separator || separator === "")
+    return getRegexWithProjectKey(projectKey, keyAnywhereInTitle);
+
+  return getRegexWithProjectKeyAndSeparator(
+    projectKey,
+    separator,
+    keyAnywhereInTitle,
+  );
+};
+
+export const getPullRequestTitle = () => {
+  const pull_request = github.context.payload.pull_request;
+  core.debug(
+    `Pull Request: ${JSON.stringify(github.context.payload.pull_request)}`,
+  );
+  if (pull_request == undefined || pull_request.title == undefined) {
+    throw new Error("This action should only be run with Pull Request Events");
+  }
+  return pull_request.title;
+};
+
+const getDefaultJiraIssueRegex = () =>
+  new RegExp(
+    "(?<=^|[a-z]\\-|[\\s\\p{Punct}&&[^\\-]])([A-Z][A-Z0-9_]*-\\d+)(?![^\\W_])(\\s)+(.)+",
+  );
+
+const isValidProjectKey = (projectKey: string) =>
+  /(?<=^|[a-z]-|[\s\p{Punct}&&[^-]])([A-Z][A-Z0-9_]*)/.test(projectKey);
+
+const getRegexWithProjectKeyAndKeyAnywhereInTitle = (
+  projectKey: string,
+  keyAnywhereInTitle: boolean,
+) =>
+  `${keyAnywhereInTitle ? "(.)*" : ""}(${
+    keyAnywhereInTitle ? "" : "^"
+  }${projectKey}-){1}`;
+
+const getRegexWithProjectKey = (
+  projectKey: string,
+  keyAnywhereInTitle: boolean,
+) =>
+  new RegExp(
+    `${getRegexWithProjectKeyAndKeyAnywhereInTitle(
+      projectKey,
+      keyAnywhereInTitle,
+    )}(\\d)+(\\s)+(.)+`,
+  );
+
+const getRegexWithProjectKeyAndSeparator = (
+  projectKey: string,
+  separator: string,
+  keyAnywhereInTitle: boolean,
+) =>
+  new RegExp(
+    `${getRegexWithProjectKeyAndKeyAnywhereInTitle(
+      projectKey,
+      keyAnywhereInTitle,
+    )}(\\d)+(${separator})+(\\S)+(.)+`,
+  );
+
+run();
